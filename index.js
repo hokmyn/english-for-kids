9addd0f8f09d8fbe15aff7c9ca15a4d1980f7b01
Denis <den_from_bykhov1988@mail.ru>
2021-02-06 21:12:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import Menu from 'components/menuComponent';
-import Switcher from 'components/switcherComponent';
-import Game from 'components/gameComponent';
+import Menu from '/components/menuComponent';
+import Switcher from '/components/switcherComponent';
+import Game from '/components/gameComponent';
 
 const menu = new Menu();
 const switcher = new Switcher();
